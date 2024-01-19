0f491a12b6db5163950fedf5e51dfaf008ff6fd5
AramisB <brendachem7@gmail.com>
2024-01-19 01:58:05 -0800

    Monty opcodes

--- m_mul.c
+++ m_mul.c
@@ -13,7 +13,7 @@ void m_mul(stack_t **stack, unsigned int line_number)
 
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't mul, stack too short", line_number);
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	first = *stack;
--- m_sub.c
+++ m_sub.c
@@ -13,7 +13,7 @@ void m_sub(stack_t **stack, unsigned int line_number)
 
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't sub, stack too short", line_number);
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	first = *stack;
